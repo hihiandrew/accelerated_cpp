14e86bb5dd802c27845159d5b8f10a790c3bc27a
hihiandrew <areis89@gmail.com>
2020-06-30 22:11:32 +1200

    fixed cmake files

--- Chapter7/Chapter7/Chapter7.cpp
+++
@@ -1,8 +0,0 @@
-#include "ch7_0.h"
-#include "ch7_0b.h"
-
-int main()
-{
-  ch7_0b(20);
-  return 0;
-}
--- Chapter7/Chapter7/ch7_0b.h
+++
@@ -1,6 +0,0 @@
-#ifndef GUARD ch7_0b_h
-#define GUARD ch7_0b_h
-
-int ch7_0b(int);
-
-#endif // !GUARD ch7_0b_h
